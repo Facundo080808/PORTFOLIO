cf1edde7fea7979be456656b1942254b34e52b40
Facundo080808 <facundo_hub@hotmail.com>
2025-07-31 18:09:28 -0300

    first commit

---
+++ src/utils/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Portfolio de Facundo, Desarrollador y programador web, con 2 años de experiencia.';
+export const SITE_DESCRIPTION = 'Contrata a Facundo, desarrollador full-stack con experiencia empresarial. Especializado en crear aplicaciones web o movil.';
+
